4be2e58a1aa2a0132c8d7fb6f5c411fe6ec7b04f
varshithab-bms <varshithab.cs23@bmsce.ac.in>
2025-04-03 15:45:22 +0530

    Update RateMonotonic.c

--- RateMonotonic.c
+++ RateMonotonic.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -13,7 +5,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 #define MAX_PROCESS 10
 
-int num_of_process;
+int num_of_process
 int execution_time[MAX_PROCESS], period[MAX_PROCESS],
     remain_time[MAX_PROCESS], deadline[MAX_PROCESS],
     remain_deadline[MAX_PROCESS];
